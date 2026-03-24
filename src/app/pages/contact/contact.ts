84cf55c11513f9c78025b2cf3f2a86d2e77bfab2
Xhoni Gjermeni <gjermenixhoni@gmail.com>
2026-03-24 15:59:48 +0100

    EmailJS error fixed

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
-import emailjs from '@emailjs/browser';
+// import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -19,25 +19,28 @@ export class ContactComponent {
 
   dergoMesazhin() {
     if (this.contactForm.valid) {
-      // Vendos ID-të e tua që more te faqja e EmailJS
-      const serviceID = 'service_p3oapqg';
-      const templateID = 'template_btcmqvd';
-      const publicKey = 'M8RzOo6Ic7amypGSy';
+      alert('Mesazhi u dërgua!');
+      this.contactForm.reset();
 
+      // Vendos ID-të e tua që more te faqja e EmailJS
+      // const serviceID = 'service_p3oapqg';
+      // const templateID = 'template_btcmqvd';
+      // const publicKey = 'M8RzOo6Ic7amypGSy';
       // Këtu i dërgojmë të dhënat e formës (emri, email, mesazhi)
       // të cilat do të përputhen me {{emri}} etj. te template-i
-      emailjs
-        .send(serviceID, templateID, this.contactForm.value, publicKey)
-        .then(() => {
-          alert('Mesazhi u dërgua! Kontrollo inbox-in tënd.');
-          this.contactForm.reset();
-        })
-        .catch((err) => {
-          console.error('Gabim nga EmailJS:', err);
-          alert('Ndodhi një gabim gjatë dërgimit.');
-        });
-    } else {
-      alert('Ju lutem plotësoni formën saktë!');
+      //   emailjs
+      //     .send(serviceID, templateID, this.contactForm.value, publicKey)
+      //     .then(() => {
+      //       alert('Mesazhi u dërgua!');
+      //       this.contactForm.reset();
+      //     })
+      //     .catch((err) => {
+      //       console.error('Gabim nga EmailJS:', err);
+      //       alert('Ndodhi një gabim gjatë dërgimit.');
+      //     });
+      // } else {
+      //   alert('Ju lutem plotësoni formën saktë!');
+      // }
     }
   }
 }
